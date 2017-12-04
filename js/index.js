98b700d856629a74b34119950ee03eaedd8852f8
PHIB16 <phib1_16@uni.worc.ac.uk>
2017-12-04 14:14:27 +0000

    hot2

--- js/index.js
+++ js/index.js
@@ -45,9 +45,29 @@ function createDialog() {
         	
 function dialogDismissed(buttonIndex) {
 	
-	if(buttonIndex==1) new Toast({content: "Have a Kit Kat then", duration: 3000});
+	if(buttonIndex==1) new Toast({content: hungryReminder(), duration: 3000});
    	else if(buttonIndex==2) new Toast({content: 'Good boi', duration: 3000});
 
+}
+
+function hungryReminder() {
+    //
+    //generate a time to post notification
+    //
+    var currentTime = new Date().getTime(); //current time
+    var notificationTime = new Date(currentTime + 30000); //delayed time  - add 1 second
+    			
+    //
+    //setup notification
+    //
+    
+    cordova.plugins.notification.local.schedule({ 
+    	id: 		1,
+        title: 		"Hey you",
+        message: 	"Get back to work, lazy boi!",
+        date: 		notificationTime, 
+        badge: 		notification_count++
+   	});
 }
 
    
